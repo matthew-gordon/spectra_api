34defc3d018f5f21bea7a7a49b80646ce5f6e970
matthew-gordon <m.gordon5525@gmail.com>
2019-02-08 00:48:17 -0700

    Initial commit

---
+++ spec/requests/todos_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'Todos API', type: :request do
+  let!(:todos) { create_list(:todo, 10) }
+  let(:todo_id) { todos.first.id }
+
+  describe 'GET /todos' do
+    before { get '/todos' }
+
+    it 'returns todos' do
+      expect(json).not_to be_empty
+      expect(json.size).to eq(10)
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
+
+end
